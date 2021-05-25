2773e3b2ba6df55b25433849f5f5bd9c2d3816c6
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2021-05-24 21:40:04 -0700

    Added base Action

---
+++ build.mjs
@@ -0,0 +1,33 @@
+// Invoke this file to build the app using esbuild
+import esbuild from 'esbuild'
+import rimraf from 'rimraf'
+
+const mode = process.env.NODE_ENV || 'production'
+const outdir = 'dist/'
+
+try {
+    /** @type esbuild.BuildOptions */
+    const esbuildOpts = {
+        entryPoints: ['src/index.ts'],
+        outfile: outdir + 'index.js',
+        bundle: true,
+        platform: 'node',
+        target: 'es2017',
+        charset: 'utf8',
+        color: true,
+        format: 'iife',
+    }
+    if (mode == 'production') {
+        // In production mode, minify the output and enable sourcemaps
+        esbuildOpts.minify = true
+        esbuildOpts.sourcemap = true
+    }
+
+    // Clean the output directory
+    rimraf.sync(outdir)
+
+    // Build with esbuild
+    await esbuild.build(esbuildOpts)
+} catch (err) {
+    process.exit(1)
+}
